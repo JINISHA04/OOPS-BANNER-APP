a259fae4acac3bb12ddc9b6700eb3eb020841ace
jinisha <js9158@srmist.edu.in>
2026-03-02 20:58:21 +0530

    UC1 : Print literal text OOPS

---
+++ OOPSBannerApp.java
@@ -0,0 +1,17 @@
+/**
+ * OOPSBannerApp
+ * 
+ * Prints the literal text "OOPS" to the console.
+ * 
+ * @author JINISHA
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    /**
+     * Main method - Entry point of the application
+     */
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
